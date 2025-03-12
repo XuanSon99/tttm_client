a3cfd188d90979c20c858a337c71bc91151f4f6b
XuanSon99 <64201776+XuanSon99@users.noreply.github.com>
2025-03-12 21:12:43 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,24 @@
+// module.exports = {
+//     apps: [
+//       {
+//         name: 'NuxtAppName',
+//         exec_mode: 'cluster',
+//         instances: 'max',
+//         script: './node_modules/nuxt/bin/nuxt.mjs',
+//         args: 'start'
+//       }
+//     ]
+//   }
+
+
 module.exports = {
-    apps: [
-      {
-        name: 'NuxtAppName',
-        exec_mode: 'cluster',
-        instances: 'max',
-        script: './node_modules/nuxt/bin/nuxt.mjs',
-        args: 'start'
-      }
-    ]
-  }
+  apps: [
+    {
+      name: 'NuxtAppName',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ]
+}
